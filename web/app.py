7c71a8b08d71fdbabc36a16bd548ecc48433a372
ChaaaChaaa <56629324+ChaaaChaaa@users.noreply.github.com>
2024-02-26 10:57:09 +0900

    app에 mysql대신 dynamodb로 변경

--- web/app.py
+++ web/app.py
@@ -1,31 +1,50 @@
-import pymysql
-from flask import Flask, jsonify
+qimport pymysql
 import os
+from flask import Flask, jsonify
+import boto3
+from botocore.exceptions import ClientError
+
 
 app = Flask(__name__)
 
-#test9
+# db_host = 'host.docker.internal'
+# #db_host = os.environ.get('MYSQL_ROOT_HOST')
+# db_host_password = os.environ.get('MYSQL_ROOT_PASSWORD')
+# db_user = os.environ.get('MYSQL_USER')
+# db_password = os.environ.get('MYSQL_PASSWORD')
+# db_name = os.environ.get('MYSQL_DATABASE')
 
-db_host = 'host.docker.internal'
-#db_host = os.environ.get('MYSQL_ROOT_HOST')
-db_host_password = os.environ.get('MYSQL_ROOT_PASSWORD')
-db_user = os.environ.get('MYSQL_USER')
-db_password = os.environ.get('MYSQL_PASSWORD')
-db_name = os.environ.get('MYSQL_DATABASE')
+dynamodb_region = os.environ.get('ap-northeast-2')
+dynamodb_table_name = os.environ.get('users_list')
+
+dynamodb = boto3.resource('dynamodb',region,name=dynamodb_region)
+table = dynamodb.Table(dynamodb_table_name)
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+# @app.route('/users')
+# def list_users():
+#     connection = pymysql.connect(host=db_host,user=db_user, password=db_password, db=db_name)
+#     cursor = connection.cursor(pymysql.cursors.DictCursor)
+#     cursor.execute("SELECT * FROM users")
+#     users = cursor.fetchall()
+#     cursor.close()
+#     connection.close()
+#     return jsonify(users)
+
 @app.route('/users')
 def list_users():
-    connection = pymysql.connect(host=db_host,user=db_user, password=db_password, db=db_name)
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT * FROM users")
-    users = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    try:
+        response = table.scan()
+        users = response['Items']
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return jsonify({"error":e.response['Error']['Message']}),500
     return jsonify(users)
 
 
